2698987072ec0003c956b4259afa78a601439699
aadesh sain <aajad.sain@peaklyft.top>
2026-02-18 17:15:10 +0530

    create build

--- src/app/solutions/blockchain/page.tsx
+++ src/app/solutions/blockchain/page.tsx
@@ -59,7 +59,7 @@ export default function SalesAcademyPage() {
         /> */}
                 {/* <ExpertsGrid data={expertsData} /> */}
 
-        <WhatsNewCards data={whatsNewDefaultData2} showHeader={false} />
+        <WhatsNewCards data={whatsNewDefaultData2} />
         
     
       </div>
--- src/app/solutions/web-development/page.tsx
+++ src/app/solutions/web-development/page.tsx
@@ -1,24 +1,16 @@
 import AcademyTabs from "@/src/components/common/AcademyTabsSection";
-import ExpertsGrid from "@/src/components/common/SalesSection/expert/expert";
-import OnboardingSection from "@/src/components/common/SalesSection/SalesSection";
 import WhatsNewCards from "@/src/components/common/WhatsNew";
 import Hero from "@/src/components/Hero/Hero";
 import WhyChoosePeaklyft from "@/src/components/whychoosePeaklyft/WhyChoosePeaklyft";
 import { salesAcademyTabs } from "@/utils/academyTabsData";
-import { expertsData } from "@/utils/expertsData";
-
 import { brandSections, heroSections } from "@/utils/Hero";
-import {
-  onboardingSection,
-  onboardingSectionData,
-} from "@/utils/salesSections";
 import { whatsNewDefaultData2 } from "@/utils/utils";
 import { whyChoosePeaklyft } from "@/utils/whychoosePeaklyft";
 
 export default function SalesAcademyPage() {
   return (
     <>
-      <div className="px-2.5">  
+      <div className="px-2.5">
         <Hero
           heroData={heroSections["sales-academy"]}
           brandData={brandSections.default}
@@ -32,40 +24,8 @@ export default function SalesAcademyPage() {
           tabs={salesAcademyTabs}
           image="/explore.png"
         />
-         
-        
-        {/* <OnboardingSection
-          title={onboardingSectionData.title}
-          subtitle={onboardingSectionData.subtitle}
-          taglines={onboardingSectionData.taglines}
-          desc={onboardingSectionData.desc}
-          cards={onboardingSectionData.cards}
-          buttonText={onboardingSectionData.buttonText}
-          variant="left"
-        />
-        <OnboardingSection
-          title={onboardingSection.title}
-          subtitle={onboardingSection.subtitle}
-          taglines={onboardingSection.taglines}
-          desc={onboardingSection.desc}
-          cards={onboardingSection.cards}
-          buttonText={onboardingSection.buttonText}
-          variant="right"
-        />
-        <OnboardingSection
-          title={onboardingSectionData.title}
-          subtitle={onboardingSectionData.subtitle}
-          taglines={onboardingSectionData.taglines}
-          desc={onboardingSectionData.desc}
-          cards={onboardingSectionData.cards}
-          buttonText={onboardingSectionData.buttonText}
-          variant="left"
-        /> */}
-                {/* <ExpertsGrid data={expertsData} /> */}
 
-        <WhatsNewCards data={whatsNewDefaultData2} showHeader={false} />
-        
-    
+        <WhatsNewCards data={whatsNewDefaultData2} />
       </div>
     </>
   );
